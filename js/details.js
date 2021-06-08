99be2178698cceb27cf3dff251038817e51ff283
Jeremy Artiflas <Artiflas@gmail.com>
2021-06-08 19:22:07 +0200

    whatever

--- js/details.js
+++ js/details.js
@@ -1,24 +1,28 @@
 // javascript for details.html
-
 const id = new URLSearchParams(window.location.search).get('id');
 const container = document.querySelector('.details');
 const deleteBtn = document.querySelector('.delete');
 
 const renderDetails = async () => {
   const res = await fetch('https://my-json-server.typicode.com/Artiflas/jsonserver/posts/' + id);
+  if (!res.ok) {
+    window.location.replace("/");
+  }
   const post = await res.json();
 
   const template = `
-  <h1>${post.title}</h1>
-  <p>${post.body}</p>  
+    <h1>${post.title}</h1>
+    <p>${post.body}</p>
   `
+
   container.innerHTML = template;
 }
-deleteBtn.addEventListener('click', async (e) => {
-  const re = await fetch('https://my-json-server.typicode.com/Artiflas/jsonserver/posts/' + id, {
+
+deleteBtn.addEventListener('click', async () => {
+  const res = await fetch('http://localhost:3000/posts/' + id, {
     method: 'DELETE'
-  })
-  window.location.replace('/index.html');
+  });
+  window.location.replace("/");
 })
 
-window.addEventListener('DOMContentLoaded', () => renderDetails());
+window.addEventListener('DOMContentLoaded', renderDetails);

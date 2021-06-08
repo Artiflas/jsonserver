78f7d42190bb8c21594c7c59c17bf826224980b4
Jeremy Artiflas <Artiflas@gmail.com>
2021-06-08 17:10:31 +0200

    continued detail.js

--- js/details.js
+++ js/details.js
@@ -1,11 +1,16 @@
 // javascript for details.html
 
 const id = new URLSearchParams(window.location.search).get('id');
+const container = document.querySelector('details');
 
 const renderDetails = async () => {
   const res = await fetch('http://localhost:5500/posts/' + id);
   const post = await res.json();
-  
+
+  const template = `
+  <h1>${post.title}</h1>
+  <p>${post.body}</p>  
+  `
 
 }
 

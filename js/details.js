b7e12c6a296d98f8f6fbcbb3f8c2be5661b57fa8
Jeremy Artiflas <Artiflas@gmail.com>
2021-06-08 17:45:42 +0200

    db.json

--- js/details.js
+++ js/details.js
@@ -4,14 +4,14 @@ const id = new URLSearchParams(window.location.search).get('id');
 const container = document.querySelector('details');
 
 const renderDetails = async () => {
-  const res = await fetch('http://localhost:5500/posts/' + id);
+  const res = await fetch('https://my-json-server.typicode.com/Artiflas/jsonserver/db' + id);
   const post = await res.json();
 
   const template = `
   <h1>${post.title}</h1>
   <p>${post.body}</p>  
   `
-
+  container.innerHTML = template;
 }
 
 window.addEventListener('DOMContentLoaded', () => renderDetails());
